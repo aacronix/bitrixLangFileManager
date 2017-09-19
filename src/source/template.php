2cd96e5545aeb8c4187a5bb652fbcbf95a53c2f4
Unknown <bacik.vas@gmail.com>
2017-09-19 22:59:56 +0300

    Оформил первоначальную конфигурацию

---
+++ src/source/template.php
@@ -0,0 +1,190 @@
+<?
+
+$MESS["RU"] = "Русский";
+$MESS["EN"] = "English";
+$MESS["EN_MENU"] = "Eng";
+
+/* GLOBAL */
+$MESS["FROM"] = "от";
+$MESS["CURRENCY"] = "руб.";
+$MESS["DATE_FORMAT"] = "d/m/Y";
+$MESS["SEE_ROOM"] = "Смотреть номера";
+
+$MESS["BOOKING_BUTTON"] = "Забронировать";
+$MESS["BOOKING_BUTTON_FULL"] = "Забронировать номер";
+$MESS["CLAIM_CONFERENCE_BUTTON"] = "Оставить заявку";
+
+$MESS["HOTEL_TYPE"] = "";
+$MESS["HOTEL_TITLE"] = "ООО «МААН»";
+$MESS["HOTEL_NAME"] = "Pricso Hotels";
+$MESS["HOTEL_GENERAL_HEADER_PHONE"] = "tel:+74959896009";
+$MESS["HOTEL_GENERAL_HEADER_PHONE_TEXT"] = "+7 (495) 989-60-09";
+$MESS["HOTEL_CONFERENCE_PRICE"] = "Стоимость аренды зала";
+$MESS["HOTEL_CONFERENCE_RUB_PER_HOUR"] = "руб./час";
+$MESS["MAP_CENTER"] = "55.681831,37.568883";
+$MESS["TL"] = "https://www.travelline.ru/";
+$MESS["TL_NAME"] = "Travelline";
+$MESS["HOTEL_DESCRIBE"] = "<h3>Гостиница</h3><h2>«Академическая»<h1>";
+
+$MESS["TO"] = "до";
+$MESS["PEOPLE"] = "человек";
+$MESS["PEOPLE_SHORT"] = "чел.";
+$MESS["MORE"] = "Подробнее";
+$MESS["HIDE"] = "Скрыть";
+$MESS["DESCRIPTION"] = "Описание";
+$MESS["BOOKING_DEPARTMENT"] = "Служба бронирования";
+$MESS["CURRENCY_RUB"] = "₽";
+$MESS["CURRENCY_USD"] = "$";
+$MESS["CURRENCY_EURO"] = "€";
+
+/* HEADER */
+//$MESS["HEADER_LOGO_ALT"] = $MESS["HOTEL_TYPE"]." «".$MESS["HOTEL_NAME"]."» ".$MESS["HOTEL_CITY"];
+$MESS["HEADER_LOGO_ALT"] = "Международное Академическое Агентство «Наука»";
+$MESS["HEADER_BOOKING_BLOCK_TITLE"] = "Отдел бронирования";
+$MESS["HEADER_ADDRESS_LABEL"] = "Адрес:";
+$MESS["HEADER_ADDRESS"] = $MESS["HOTEL_COUNTRY"] . ", " . $MESS["HOTEL_REGION"] . ",<br>" . $MESS["HOTEL_CITY"] . ", " . $MESS["HOTEL_STREET"] . ", " . $MESS["HOTEL_HOUSE"];
+$MESS["HEADER_PHONE_LINK"] = $MESS["HOTEL_PHONE"];
+$MESS["HEADER_PHONE_TEXT"] = $MESS["HOTEL_PHONE_TEXT"];
+$MESS["HEADER_PHONE_LABEL"] = "Телефон:";
+$MESS["HEADER_PHONE_INFO"] = "(Звонок по России бесплатный)";
+$MESS["HEADER_EMAIL_LINK"] = $MESS["HOTEL_EMAIL"];
+$MESS["HEADER_EMAIL_TEXT"] = $MESS["HEADER_EMAIL_LINK"];
+$MESS["HEADER_MENU_WORD"] = "Меню";
+
+/* FOOTER */
+$MESS["FOOTER_HOTEL_NAME"] = $MESS["HOTEL_TYPE"] . " <span class=\"hotel-name\">" . $MESS["HOTEL_NAME"] . "</span>";
+$MESS["FOOTER_OFFICIAL"] = "© " . $MESS["HOTEL_TITLE"] . " " . date('Y') . ". Официальный сайт";
+$MESS["FOOTER_ADDRESS"] = $MESS["HOTEL_CITY"] . ", " . $MESS["HOTEL_STREET"] . ", " . $MESS["HOTEL_HOUSE"];
+$MESS["FOOTER_PHONE"] = $MESS["HOTEL_PHONE"];
+$MESS["FOOTER_PHONE_TEXT"] = $MESS["HOTEL_PHONE_TEXT"];
+$MESS["FOOTER_HOTELS_LABEL"] = "Наши отели";
+$MESS["FOOTER_WEATHER"] = "Погода";
+$MESS["FOOTER_TIME"] = "Время";
+$MESS["FOOTER_DEV_DESIGN"] = "Разработка и дизайн";
+
+$MESS["FOOTER_SOCIAL_VK"] = "";
+$MESS["FOOTER_SOCIAL_OK"] = "";
+$MESS["FOOTER_SOCIAL_FB"] = "";
+$MESS["FOOTER_SOCIAL_TW"] = "";
+$MESS["FOOTER_SOCIAL_IN"] = "";
+$MESS["FOOTER_SOCIAL_VB"] = "";
+
+/* MAIN PAGE */
+$MESS["MAIN_ADVANTAGES_HEADING"] = "Наши преимущества";
+$MESS["MAIN_CATERING"] = "Рестораны и бары";
+$MESS["MAIN_CONFERENCE_SERVICES"] = "Конференц услуги";
+$MESS["MAIN_ROOMS_HEADING"] = "Номера и цены";
+$MESS["MAIN_OFFERS_HEADING"] = "Спецпредложения";
+$MESS["MAIN_RESTAURANTS_HEADING"] = "Рестораны и бары";
+$MESS["MAIN_CONFERENCE_HEADING"] = "Конференц-залы";
+$MESS["MAIN_SERVICES_HEADING"] = "Услуги";
+$MESS["MAIN_CONTACTS_HEADING"] = "Контакты";
+$MESS["MAIN_HOTEL_COST_LABEL"] = "Стоимость размещения";
+$MESS["MAIN_HOTEL_3D_TEXT"] = "3d-тур";
+
+/* MOBILE MENU */
+$MESS["MM_CHANGE_HOTEL"] = "Выбрать другой отель";
+$MESS["MM_ALL_HOTELS"] = "Все отели";
+$MESS["MM_ABOUT"] = "О гостинице";
+$MESS["MM_ROOMS"] = "Номера и цены";
+$MESS["MM_SERVICES"] = "Услуги";
+$MESS["MM_OFFERS"] = "Спецпредложения";
+$MESS["MM_REVIEWS"] = "Отзывы";
+$MESS["MM_CONTACTS"] = "Контакты";
+
+/*404*/
+$MESS["404_HEADING"] = "404: Страница не найдена или была удалена";
+$MESS["404_CONTENT"] = "<p>Вы можете перейти на <a href=\"/\">Главную страницу</a>, перейти к <a href=\"/booking/\">бронированию номера</a> или выбрать интересующий вас раздел.</p>";
+
+
+$MESS["KITCHEN_DETAIL_EQUIPMENT"] = "Оснащение кухни";
+$MESS["KITCHEN_DETAIL_DISHES"] = "Посуда";
+$MESS["KITCHEN_DETAIL_OTHER"] = "Другое";
+
+/* ROOMS */
+//$MESS["ROOM_CURRENCY_TIME"] = $MESS["CURRENCY"]."/сут.";
+$MESS["ROOM_BOOKING_BUTTON"] = "Забронировать";
+$MESS["ROOM_BOOKING_BUTTON_FULL"] = "Забронировать номер";
+$MESS["ROOM_DETAIL_DESCRIPTION"] = "Описание";
+$MESS["ROOM_DETAIL_ADDITIONAL"] = "Дополнительно";
+$MESS["ROOM_DETAIL_EQUIPMENT"] = "В номере";
+$MESS["ROOM_DETAIL_BATHROOM"] = "В ванной комнате";
+$MESS["ROOM_DETAIL_KITCHEN"] = "Кухня";
+$MESS["ROOM_PRICE_LABEL"] = "Стоимость размещения:";
+$MESS["ROOM_PRICE_SINGLE_LABEL"] = "Размещение от:";
+$MESS["ROOM_PRICE_DOUBLE_LABEL"] = "Двухместное размещение";
+$MESS["ROOM_PRICE_TRIPPLE_LABEL"] = "Трехместное размещение";
+$MESS["ROOM_PRICE_INFO"] = "* Данная цена не является публичной офертой и может меняться. Просим уточнять цены при бронировании на конкретный период.";
+
+/* NEWS */
+$MESS["NEWS_MAIN_HEADING"] = "Новости";
+$MESS["NEWS_FORMAT"] = $MESS["DATE_FORMAT"];
+$MESS["NEWS_MORE_LINK"] = "Подробнее";
+
+/* OFFERS */
+$MESS["OFFER_BOOKING_BUTTON"] = "Забронировать по акции";
+
+/* REVIEWS */
+$MESS["REVIEWS_MAIN_HEADING"] = "Отзывы";
+$MESS["REVIEWS_MORE_LINK"] = "Подробнее";
+
+$MESS["REVIEWS_FORM_HEADING"] = "Добавить отзыв";
+$MESS["REVIEWS_LABEL_NAME"] = "Ваше имя";
+$MESS["REVIEWS_LABEL_CITY"] = "Откуда Вы";
+$MESS["REVIEWS_LABEL_EMAIL"] = "E-mail";
+$MESS["REVIEWS_LABEL_MESSAGE"] = "Сообщение";
+$MESS["REVIEWS_FORM_BUTTON"] = "Оставить отзыв";
+$MESS["REVIEWS_ATTENTION"] = "* Отзывы работают в режиме модерирования.<br>** Все поля обязательны к заполнению.";
+
+$MESS["FORM_STATE_SUCCESS"] = "Спасибо! Ваш отзыв будет опубликован после проверки.";
+$MESS["FORM_STATE_NO_DATA"] = "Ошибка, обязательные поля не заполнены";
+$MESS["FORM_STATE_VALIDATION_ERROR"] = "Ошибка, проверьте введенные данные";
+$MESS["FORM_STATE_SAVE_ERROR"] = "Ошибка сохранения, попробуйте позднее";
+
+/* SERVICES */
+$MESS["SERVICE_VIEW_ALL"] = "Просмотреть все услуги";
+$MESS["SERVICE_DESCRIPTION"] = "Описание";
+$MESS["SERVICE_WORKTIME"] = "Время работы:";
+$MESS["SERVICE_FREE"] = "Бесплатные";
+$MESS["SERVICE_PAID"] = "За дополнительную плату";
+
+/*CONFERENCE MAIN*/
+
+$MESS["MAIN_CONFERENCE_SHOW_ALL"] = "Смотреть залы";
+$MESS["MAIN_CONFERENCE_AKADEM_TITLE"] = "Конференц-залы<br>«Академическая»";
+$MESS["MAIN_CONFERENCE_YZKOE_TITLE"] = "Конференц-залы<br>«Узкое»";
+
+/*CONFERENCE*/
+$MESS["CONFERENCE_EQUIPMENT_IN_HALL"] = "Оборудование в зале";
+$MESS["CONFERENCE_CAPACITY_AND_SEAT_TYPES"] = "Вместимость и варианты рассадки";
+$MESS["CONFERENCE_THEATER_SEAT_TYPE"] = "Театр";
+$MESS["CONFERENCE_CLASS_SEAT_TYPE"] = "Класс";
+$MESS["CONFERENCE_CIRCLE_SEAT_TYPE"] = "Круглый стол";
+$MESS["CONFERENCE_USHAPE_SEAT_TYPE"] = "П-образный";
+$MESS["CONFERENCE_SQUARE_SEAT_TYPE"] = "Карэ";
+
+/*other*/
+$MESS["AREA_m_2"] = "м<sup>2</sup>";
+$MESS["HOLIDAY_PRICE"] = "Данная цена действительна в выходные и праздничные дни.";
+
+$MESS["HOTEL_ROOMS_TITLE"] = "Номерной фонд";
+$MESS["HOTEL_CONFERENCE_TITLE"] = "Конференц-залы";
+$MESS["HOTEL_OFFERS_TITLE"] = "Специальные предложения";
+$MESS["HOTEL_RESTAURANTS_TITLE"] = "Рестораны и бары";
+
+$MESS["HOTEL_INDEX_ROOMS_TITLE"] = "Номерной фонд";
+$MESS["HOTEL_OTHER_ROOMS_TITLE"] = "Другие номера";
+$MESS["HOTEL_INDEX_CONFERENCE_TITLE"] = "Конференц-залы";
+$MESS["HOTEL_INDEX_OFFERS_TITLE"] = "Спецпредложения";
+$MESS["HOTEL_INDEX_RESTAURANTS_TITLE"] = "Рестораны";
+$MESS["HOTEL_INDEX_REVIEWS_TITLE"] = "Отзывы";
+$MESS["HOTEL_INDEX_NEWS_TITLE"] = "Новости";
+
+
+/*Akadem*/
+
+$MESS["AKADEMICAL_HOTEL_CITY"] = "г. Москва";
+$MESS["AKADEMICAL_HOTEL_STREET"] = "ул. Донская";
+$MESS["AKADEMICAL_HOTEL_BUILD"] = "д. 1";
+$MESS["AKADEMICAL_HOTEL_METRO"] = "ст. метро «Октябрьская»";
+$MESS["AKADEMICAL_FULL_ADDRESS"] = $MESS["AKADEMICAL_HOTEL_CITY"] . ", " . $MESS["AKADEMICAL_HOTEL_STREET"] . ", " . $MESS["AKADEMICAL_HOTEL_BUILD"]. " (" . $MESS["AKADEMICAL_HOTEL_METRO"] . " )";
